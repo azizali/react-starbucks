a60e988b39ffb1546b97beeb6445c83e613898bd
Aziz Ali <aziz@ilovecoding.org>
2020-03-09 11:44:20 -0700

    First Comonent and Default/Named Import/Export

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App(){
+  return (
+    <div>
+      This is some info
+    </div>
+  )
+}
+
+export function util (a, b){
+  return a+ b
+}
+
+export function util2 (a, b){
+  return a+ b
+}
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import App, { util, util2, xyz } from './App';
+
+// function FirstComponent(){
+//   return (
+//     <h1>Hello World</h1>
+//   )
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
