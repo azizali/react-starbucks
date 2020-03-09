2cced3c3e0480b6dcbe8b26ed614859a5cf1d4d7
Aziz Ali <aziz@ilovecoding.org>
2020-03-09 14:07:02 -0700

    Props in detail

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ export default function App(){
   // React Fragment
   return (
     <React.Fragment>
-      <Header />
+      <Header
+        // name="Starbucks"
+        age={35}
+        info={{
+          "address": "Seattle, WA",
+          employeeCount: 5000,
+          anotherFunc: ()=>{
+            return 'something else'
+          }
+        }}
+        callMe = {()=>{
+          return '1.800.123.4567'
+        }}
+        friends={["A", "B","C"]}
+      />
       <p>This is some info</p>
       <div>
         <Content />
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+// // Object Destructing
+// const obj = {
+//   name: 'A',
+//   age: 2,
+//   phone: 3
+// }
 
-export default function Header(){
+// // Without Object Destructuring
+// const name = obj.name
+// const age = obj.age
+// const phone = obj.phone
+
+// // Object Destructuring
+// const { name, age, phone } = obj
+
+export default function Header({ name, callMe, age, friends, info }){
+  // Do not do this
+  // age = 36;
+  const phone = callMe()
   return (
-    <div>I am Header</div>
+    <div>
+      Ternary Operator: {name? 'Hi name': 'Name Not found'} <br />
+      Calc: {1 + 1 / 3} <br />
+      Name: {name}<br/>
+      Age: {age}<br/>
+      friends: {friends}<br/>
+      phone: {callMe() || phone}<br/>
+      Address: {info.address}<br/>
+      EmployeeCount: {info.employeeCount}<br/>
+
+      {/* TROUBLESHOOTING HACK */}
+      <pre>
+        {JSON.stringify(info, null, 4)}
+      </pre>
+    </div>
   )
 }
