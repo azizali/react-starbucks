812fadae5745c62fc747aa78e3b0a35de07c73c4
Aziz Ali <aziz@ilovecoding.org>
2020-03-10 09:52:12 -0700

    Styling components with class and inline-css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,17 @@
+import './footer.css';
 import React from 'react';
 
 export default function Footer(){
   return (
-    <div>I am Footer</div>
+    // <div style="background-color:green">Pure HTML</div>
+    <div style={{
+        backgroundColor : 'green',
+        color: 'red',
+        margin: '10px 20px 30px',
+        padding: '10px 20px 30px'
+      }}
+    >
+      Pure JSX
+    </div>
   )
 }
